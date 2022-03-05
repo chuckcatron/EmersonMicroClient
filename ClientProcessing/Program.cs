b39f9dd1ef37744315bfbd40918c63093cce7463
Chuck Catron <charles.catron@effectual.com>
2022-03-05 18:34:53 -0500

    Add project files.

---
+++ ClientProcessing/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace SubscriberA
+{
+    class Program
+    {
+        public static IList<string> allowableCommandLineArgs
+            = new[] {"Sports", "News", "All"};
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1 || !allowableCommandLineArgs.Contains(args[0]))
+            {
+                Console.WriteLine("Expected one argument, either " +
+                                  "'Sports', 'News' or 'All'");
+                Environment.Exit(-1);
+            }
+
+            string topic = args[0] == "All" ? "" : args[0];
+            Console.WriteLine("Subscriber started for Topic : {0}", topic);
+            using (var subSocket = new SubscriberSocket())
+            {
+                subSocket.Options.ReceiveHighWatermark = 1000;
+                subSocket.Connect("tcp://127.0.0.1:3000");
+                subSocket.Subscribe(topic);
+                Console.WriteLine("Subscriber socket connecting...");
+                while (true)
+                {
+                    string messageTopicReceived = subSocket.ReceiveFrameString();
+                    string messageReceived = subSocket.ReceiveFrameString();
+                    Console.WriteLine(messageReceived);
+                }
+            }
+        }
+    }
+}
